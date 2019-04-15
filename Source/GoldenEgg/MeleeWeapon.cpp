0bdce14be1d6d50850a8aae7d8e91858e43910c9
Robert Hinds <rhinds1004@gmail.com>
2019-04-14 20:25:00 -0700

    added sword models. add melee weapon class.

---
+++ Source/GoldenEgg/MeleeWeapon.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MeleeWeapon.h"
+
+
+// Sets default values
+AMeleeWeapon::AMeleeWeapon()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMeleeWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMeleeWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
