bdd2ba70565170f039643574633d9768e2878056
rhinds1004 <rhinds1004@gmail.com>
2019-05-06 21:36:38 -0700

    Added fire projectile class. currently nothing there

---
+++ Source/GoldenEgg/FireProjectile.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Projectile.h"
+#include "FireProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GOLDENEGG_API AFireProjectile : public AProjectile
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
