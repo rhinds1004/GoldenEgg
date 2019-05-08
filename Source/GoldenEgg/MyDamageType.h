009e83fb111e9ce08c3bec93c3085d8d86bf7304
rhinds1004 <rhinds1004@gmail.com>
2019-05-07 20:58:26 -0700

    messing around with damagetypes. works but had to make a decrive class of UDamageType then do a cast. seems very ugly... this probably should of been a branch.

---
+++ Source/GoldenEgg/MyDamageType.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DamageType.h"
+#include "MyDamageType.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GOLDENEGG_API UMyDamageType : public UDamageType
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
