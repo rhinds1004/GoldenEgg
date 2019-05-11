2154fe89d177939bd0ed317389e13fcb2c3db8b6
rhinds1004 <rhinds1004@gmail.com>
2019-05-10 19:52:18 -0700

    create monster ai c++ class. moved logic to run towards the player into there.  MoveToLocation uses the nav-mesh to navigate a path to the target vector.

---
+++ Source/GoldenEgg/MonsterAIController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "MonsterAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GOLDENEGG_API AMonsterAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
