05b29cf3034f74fa3d0c124ef5070238e1f982fd
rhinds1004 <rhinds1004@gmail.com>
2019-05-12 12:06:11 -0700

    added blackboard and behavior tree class files for monsters. Changed tick function monsterbase to use behavior tree.

---
+++ Source/GoldenEgg/BTTask_FollowPlayer.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_FollowPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GOLDENEGG_API UBTTask_FollowPlayer : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
