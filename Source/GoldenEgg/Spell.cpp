2a1a7efc780cd3851944dfaf59e3d3379b30dfaf
rhinds1004 <rhinds1004@gmail.com>
2019-05-17 16:38:50 -0700

    Added particle effect for blizzard spell. Added spell class that will be used to blueprint spells for the player to cast.

--- Source/GoldenEgg/Spell.cpp
+++ Source/GoldenEgg/Spell.cpp
@@ -1,15 +1,32 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Spell.h"
-
+#include "MonsterBase.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
-ASpell::ASpell()
+ASpell::ASpell(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	ProxBox = ObjectInitializer.CreateDefaultSubobject<UBoxComponent>(this, TEXT("ProxBox"));
+	Particles = ObjectInitializer.CreateDefaultSubobject<UParticleSystemComponent>(this, TEXT("ParticleSystem"));
+	// The Particles are the root component, and the ProxBox
+	// is a child of the Particle system.
+	// If it were the other way around, scaling the ProxBox
+	// would also scale the Particles, which we don't want
+	RootComponent = Particles;
+	ProxBox->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepWorldTransform);
+
+	Duration = 3.f;
+	DamagePerSecond = 1.f;
+	TimeAlive = 0;
+
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
+
+
 
 // Called when the game starts or when spawned
 void ASpell::BeginPlay()
@@ -22,6 +39,39 @@ void ASpell::BeginPlay()
 void ASpell::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	// search the proxbox for all actors in the volume.
+	TArray<AActor*> actors;
+	ProxBox->GetOverlappingActors(actors);
+	// damage each actor the box overlaps
+	for (int c = 0; c < actors.Num(); c++)
+	{
+		// don't damage the spell caster
+		if (actors[c] != Caster)
+		{
+			// Only apply the damage if the box is overlapping
+			// the actors ROOT component.
+			// This way damage doesn't get applied for simply
+			// overlapping the SightSphere of a monster
+			AMonsterBase *monster = Cast<AMonsterBase>(actors[c]);
+			if (monster && ProxBox->IsOverlappingComponent(Cast<UPrimitiveComponent>(monster->GetCapsuleComponent())))
+			{
+				monster->TakeDamage(DamagePerSecond*DeltaTime,
+					FDamageEvent(), 0, this);
+			}
+			// to damage other class types, try a checked cast
+			// here..
+		}
+	}
+	TimeAlive += DeltaTime;
+	if (TimeAlive > Duration)
+	{
+		Destroy();
+	}
 
 }
 
+void ASpell::SetCaster(AActor* caster)
+{
+	Caster = caster;
+	RootComponent->AttachToComponent(caster->GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+}
--- Source/GoldenEgg/Spell.h
+++ Source/GoldenEgg/Spell.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "Runtime/Engine/Classes/Particles/ParticleSystemComponent.h"
 #include "Spell.generated.h"
 
 UCLASS()
@@ -13,7 +15,29 @@ class GOLDENEGG_API ASpell : public AActor
 	
 public:	
 	// Sets default values for this actor's properties
-	ASpell();
+	ASpell(const FObjectInitializer& ObjectInitializer);
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Spell)
+		UBoxComponent* ProxBox;
+	//particle visualization of the spell
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Spell)
+		UParticleSystemComponent* Particles;
+
+	//How much damage the spell does per second
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Spell)
+		float DamagePerSecond;
+
+	//How long the spell does lasts
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Spell)
+		float Duration;
+
+	//Length of the time spell has been alive in the level
+	float TimeAlive;
+
+	//caster of the spell
+	AActor* Caster;
+
+	//Parents this spell to a caster actor
+	void SetCaster(AActor* caster);
 
 protected:
 	// Called when the game starts or when spawned
