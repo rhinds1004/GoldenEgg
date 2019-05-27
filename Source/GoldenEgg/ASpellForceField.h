b77c00aeb18f68793dfbba196a16bc77cf0ff02b
rhinds1004 <rhinds1004@gmail.com>
2019-05-26 22:59:42 -0700

    add forcespell

---
+++ Source/GoldenEgg/ASpellForceField.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Spell.h"
+#include "ASpellForceField.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GOLDENEGG_API AASpellForceField : public ASpell
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
